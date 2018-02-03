240d2f41f771d54f2e120d1195babedb54dcbcc7
ruby -v <15h61a0530@cvsr.ac.in>
2018-02-03 22:09:37 +0530

    Commit3

--- app/src/main/java/com/example/mravi/collapsingtoolbar/Zones.java
+++ app/src/main/java/com/example/mravi/collapsingtoolbar/Zones.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.mravi.collapsingtoolbar.branch.CseActivity;
 
@@ -13,8 +15,22 @@ public class Zones extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zones);
+
+
     }
-   public  void department(View view){
+
+
+    public void go1(View view){
+        Toast.makeText(this, "hello clicked", Toast.LENGTH_SHORT).show();
+
+        Intent i=new Intent(this,MainActivity.class);
+        startActivity(i);
+
+
+    }
+
+
+    public  void department(View view){
         Intent dept=new Intent(this, MainActivity.class);
         startActivity(dept);
     }
